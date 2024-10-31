8c456e35e93e52e56f94a90106d19513b99ab078
vhoang11902 <vhoang11902@gmail.com>
2024-10-31 15:40:16 +0700

    Remove sensitive file and update config

--- SocialNetWork-react/src/firebase/config.js
+++ SocialNetWork-react/src/firebase/config.js
@@ -9,15 +9,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB0wNv809uVtgp3TVtQ4lkDPacUPz41gmw",
-  authDomain: "social-network-former-member.firebaseapp.com",
-  projectId: "social-network-former-member",
-  storageBucket: "social-network-former-member.appspot.com",
-  messagingSenderId: "789110003997",
-  appId: "1:789110003997:web:450180ed2e1cbc1fb8e9f4",
-  measurementId: "G-X4VM6BEVXL"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB0wNv809uVtgp3TVtQ4lkDPacUPz41gmw",
+//   authDomain: "social-network-former-member.firebaseapp.com",
+//   projectId: "social-network-former-member",
+//   storageBucket: "social-network-former-member.appspot.com",
+//   messagingSenderId: "789110003997",
+//   appId: "1:789110003997:web:450180ed2e1cbc1fb8e9f4",
+//   measurementId: "G-X4VM6BEVXL"
+// };
 
 
 // Initialize Firebase
